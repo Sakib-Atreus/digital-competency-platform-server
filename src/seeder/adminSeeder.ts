840f101014ccdd0dd0808e92c6548045125097ed
sakib-atreus <sakibatreus@gmail.com>
2025-08-08 23:29:53 +0600

    digital competency server

---
+++ src/seeder/adminSeeder.ts
@@ -0,0 +1,29 @@
+import { userRole } from '../constents';
+import { UserModel } from '../modules/user/user.model';
+import userServices from '../modules/user/user.service';
+
+const adminSeeder = async () => {
+  const admin = {
+    name: 'Admin',
+    email: 'admin@gmail.com',
+    phone: '+8801234567890',
+    role: userRole.admin,
+    password: '1',
+    aggriedToTerms:true
+  };
+  
+  const adminExist = await UserModel.findOne({ role: userRole.admin });
+
+  if (!adminExist) {
+    console.log('seeding admin....');
+    const createAdmin = await userServices.createUser(admin);
+    if (!createAdmin) 
+    {
+      throw Error('admin could not be created');
+    }
+
+    console.log("Create admin : ", createAdmin);
+  }
+};
+
+export default adminSeeder;
